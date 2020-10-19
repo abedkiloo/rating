36d668fa41271217fee89895ca6a52133762c525
Abednego Wambua <abedxh@gmail.com>
2020-10-19 15:39:04 +0300

    date format

--- app/Models/UserRating.php
+++ app/Models/UserRating.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class UserRating extends Model
@@ -9,13 +10,23 @@ class UserRating extends Model
 
     protected $table = 'rating';
     protected $fillable = [
-        'user_id', 'user_rated_id', 'rating_score'
+        'user_id', 'user_rated_id', 'rating_score', 'created_at'
     ];
     protected $appends = ['average_user_rating'];
 
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->format('d/m/Y');
+    }
+
+    public function getUpdatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->format('d/m/Y');
+    }
+
     public function getAverageUserRatingAttribute()
     {
-       return $user_rating = UserRating::where('user_rated_id')->sum('rating_score');
+        return $user_rating = UserRating::where('user_rated_id')->sum('rating_score');
     }
 
     public function user_rating()
